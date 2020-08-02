bb22cf4d1e26b64e893fce9c98efed1fa35c6840
Luke McHale <luke.mchale@protonmail.com>
2020-08-02 12:41:56 -0400

    Buffering 60 minutes of measurements in ram before appending to data file to avoid punishing flash storage. Added flush_data method to write all measurements kept in ram out to the data file. Capturing SIGINT to call flush_data before exiting.

--- main.cpp
+++ main.cpp
@@ -10,11 +10,43 @@
 
 #include <cstdlib>
 #include <ctime>
+#include <csignal>
 #include "libspi-bme680.h"
 
 namespace fs = std::filesystem;
 using system_clock = std::chrono::system_clock;
 using time_point = std::chrono::system_clock::time_point;
+using data_point = std::pair<time_point, Measurement>;
+
+// Globals:
+std::vector<data_point> measurements;
+std::ofstream out;
+
+// Forward Declarations:
+extern "C" {
+void sig_handler(int s);
+}
+
+void flush_data();
+
+
+// Definitions:
+void sig_handler(int s) {
+	std::cerr << "Caught signal " << s << std::endl;
+	flush_data();
+	std::exit(EXIT_SUCCESS);
+}
+
+
+void flush_data() {
+	for (const auto [t, m] : measurements) {
+		const std::time_t t_c = system_clock::to_time_t(t);
+		out << std::put_time(std::localtime(&t_c), "%F %T,") << m << '\n';
+	}
+	measurements.clear();
+	out.flush();
+}
+
 
 int main (int argc, char* argv[]) {
 	using namespace std::literals::chrono_literals;
@@ -32,19 +64,32 @@ int main (int argc, char* argv[]) {
 
 	// Append to sensor output file:
 	bool setupDataFile = !fs::exists(dataFile);
-	std::ofstream out(dataFile, std::ios_base::app);
-	if (setupDataFile)
+	out.open(dataFile, std::ios_base::app);
+	if (setupDataFile) {
 		out << "Time,Temperature (C),Pressure (hPa),Relative Humidity (%),Gas Sensor (ohms)\n";
+	}
+
+	// Register SIGINT handler for clean shutdown:
+	struct sigaction sigIntHandler;
+	sigIntHandler.sa_handler = sig_handler;
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
+	sigaction(SIGINT, &sigIntHandler, NULL);
 
 	// Sensor measurement loop:
+	measurements.reserve(60);
 	time_point next = std::chrono::floor<std::chrono::minutes>(start) + 1min;
 	for (;;) {
 		std::this_thread::sleep_until(next);
-		next += 1min;
-		const Measurement m = sensor.measure();
 
-		const std::time_t t_c = system_clock::to_time_t(system_clock::now());
-		out << std::put_time(std::localtime(&t_c), "%F %T,") << m << std::endl;
+		// Perform measurement:
+		measurements.emplace_back(next, sensor.measure());
+		next += 1min;
+
+		// Flush measurements to file:
+		if (measurements.size() >= 60) {
+			flush_data();
+		}
 	}
 }
 
